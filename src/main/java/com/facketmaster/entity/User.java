8b1864ace7be1c1033ce6cad838e9794754a1cb5
Eduardo Zacchi <edzacchi@gmail.com>
2026-05-23 19:58:58 -0300

    feat: add user entity and repository

---
+++ src/main/java/com/facketmaster/entity/User.java
@@ -0,0 +1,31 @@
+package com.facketmaster.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
+}
